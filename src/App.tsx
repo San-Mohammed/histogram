7018abd3a0c5f332481ce4ec12c0e3796bdb848f
San <warcommandar455@gmail.com>
2026-02-10 16:17:49 +0300

    solved histogram but not efficient but i would say its okay!!

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,114 @@ function App() {
   const [letters, setLetters] = useState<
     { letter_name: string; count: number }[]
   >([
-    { letter_name: "A", count: 0 },
-    { letter_name: "B", count: 0 },
-    { letter_name: "C", count: 0 },
-    { letter_name: "D", count: 0 },
-    { letter_name: "E", count: 0 },
-    { letter_name: "F", count: 0 },
-    { letter_name: "G", count: 0 },
-    { letter_name: "H", count: 0 },
-    { letter_name: "I", count: 0 },
-    { letter_name: "J", count: 0 },
-    { letter_name: "K", count: 0 },
-    { letter_name: "L", count: 0 },
-    { letter_name: "M", count: 0 },
-    { letter_name: "N", count: 0 },
-    { letter_name: "O", count: 0 },
-    { letter_name: "P", count: 0 },
-    { letter_name: "Q", count: 0 },
-    { letter_name: "R", count: 0 },
-    { letter_name: "S", count: 0 },
-    { letter_name: "T", count: 0 },
-    { letter_name: "U", count: 0 },
-    { letter_name: "V", count: 0 },
-    { letter_name: "W", count: 0 },
-    { letter_name: "X", count: 0 },
-    { letter_name: "Y", count: 0 },
-    { letter_name: "Z", count: 0 },
+    { letter_name: "a", count: 0 },
+    { letter_name: "b", count: 0 },
+    { letter_name: "c", count: 0 },
+    { letter_name: "d", count: 0 },
+    { letter_name: "e", count: 0 },
+    { letter_name: "f", count: 0 },
+    { letter_name: "g", count: 0 },
+    { letter_name: "h", count: 0 },
+    { letter_name: "i", count: 0 },
+    { letter_name: "j", count: 0 },
+    { letter_name: "k", count: 0 },
+    { letter_name: "l", count: 0 },
+    { letter_name: "m", count: 0 },
+    { letter_name: "n", count: 0 },
+    { letter_name: "o", count: 0 },
+    { letter_name: "p", count: 0 },
+    { letter_name: "q", count: 0 },
+    { letter_name: "r", count: 0 },
+    { letter_name: "s", count: 0 },
+    { letter_name: "t", count: 0 },
+    { letter_name: "u", count: 0 },
+    { letter_name: "v", count: 0 },
+    { letter_name: "w", count: 0 },
+    { letter_name: "x", count: 0 },
+    { letter_name: "y", count: 0 },
+    { letter_name: "z", count: 0 },
   ]);
-  return <div></div>;
+  const [inputText, setInputText] = useState<string>("");
+  const handleSubmit = () => {
+    const letters = [
+      { letter_name: "a", count: 0 },
+      { letter_name: "b", count: 0 },
+      { letter_name: "c", count: 0 },
+      { letter_name: "d", count: 0 },
+      { letter_name: "e", count: 0 },
+      { letter_name: "f", count: 0 },
+      { letter_name: "g", count: 0 },
+      { letter_name: "h", count: 0 },
+      { letter_name: "i", count: 0 },
+      { letter_name: "j", count: 0 },
+      { letter_name: "k", count: 0 },
+      { letter_name: "l", count: 0 },
+      { letter_name: "m", count: 0 },
+      { letter_name: "n", count: 0 },
+      { letter_name: "o", count: 0 },
+      { letter_name: "p", count: 0 },
+      { letter_name: "q", count: 0 },
+      { letter_name: "r", count: 0 },
+      { letter_name: "s", count: 0 },
+      { letter_name: "t", count: 0 },
+      { letter_name: "u", count: 0 },
+      { letter_name: "v", count: 0 },
+      { letter_name: "w", count: 0 },
+      { letter_name: "x", count: 0 },
+      { letter_name: "y", count: 0 },
+      { letter_name: "z", count: 0 },
+    ];
+    const trimmedString: string[] = inputText.toLowerCase().split("");
+
+    const newUpdatedLetters = letters.map((eachLetter) => {
+      let hasLetter: number = 0;
+      trimmedString.forEach((eachTrimmedLetter) => {
+        if (eachLetter.letter_name === eachTrimmedLetter) {
+          hasLetter = hasLetter + 1;
+        }
+      });
+      if (hasLetter > 0) {
+        return {
+          letter_name: eachLetter.letter_name,
+          count: hasLetter,
+        };
+      } else {
+        return eachLetter;
+      }
+    });
+    console.log(newUpdatedLetters);
+
+    setLetters(newUpdatedLetters);
+  };
+
+  return (
+    <div className=" h-screen flex justify-center items-center">
+      <div className="flex flex-col gap-3.5">
+        <div className="flex gap-2.5">
+          <input
+            type="text"
+            className="bg-amber-300"
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+          />
+          <button onClick={handleSubmit}>Submit</button>
+        </div>
+        <div className="flex gap-10">
+          {letters.map((letter, index) => {
+            return (
+              <div key={index} className="flex flex-col ">
+                <div
+                  className="w-3  bg-amber-500"
+                  style={{ height: 10 * letter.count }}
+                ></div>
+                <span>{letter.letter_name}</span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
